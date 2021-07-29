846d3620227189e47c67949cca3deff9423d9714
GermanBread <nd01@online.de>
2021-07-29 21:51:41 +0200

    new changelog! do not auto-merge files

--- src/bin/update-arnix.sh
+++ src/bin/update-arnix.sh
@@ -18,6 +18,8 @@ log "Installing dependencies"
     pacman -S --noconfirm --needed --asdeps gzip 1>/dev/null
 [ -z "$(command -v tar)" ] && \
     pacman -S --noconfirm --needed --asdeps tar 1>/dev/null
+[ -z "$(command -v less)" ] && \
+    pacman -S --noconfirm --needed --asdeps less 1>/dev/null
 
 log "Downloading update for branch '${_branch_preset}', URL '${_update_source}'"
 mkdir -p /tmp/arnix-update
@@ -30,17 +32,14 @@ fi
 gunzip bootstrap.tar.gz
 tar xf bootstrap.tar
 
+less changelog.txt
 rm -rf /arnix/merge
-mkdir -p /arnix/merge/etc
-cp -a bin/* /arnix/bin
-cp -a etc/os-release /arnix/etc/os-release
-cp -a etc/0-arnix.hook /arnix/etc/0-arnix.hook
-cp -a etc/arnix.conf /arnix/merge/etc/arnix.conf
-cd /arnix/
-cat /tmp/arnix-update/changelog.txt
+mkdir -p /arnix/merge
+cp -a bin/* /arnix/merge/bin
+cp -a etc* /arnix/merge/etc
 rm -r /tmp/arnix-update
 
-warning 'Manual intervention is required - files need to be merged'
-warning '/arnix/merge/etc/arnix.conf -> /arnix/etc/arnix.conf'
+warning "Manual intervention is required - files need to be merged. Because guess what, merging updates is harder than it sounds."
+warning '/arnix/merge/ -> /arnix'
 warning 'Delete /arnix/merge when you are done'
 exit
