c02bc87e93146caa43f05ceaa624f4a3258b2b6c
GermanBread <nd01@online.de>
2021-08-09 14:10:40 +0200

    fix init hooks should now exist

--- src/bin/create-generation.sh
+++ src/bin/create-generation.sh
@@ -9,7 +9,8 @@ _current_generation=$(readlink /arnix/generations/current)
 _next_generation=$((${_current_generation} + 1))
 
 if [[ $@ != '*nocopy*' ]] 2>/dev/null || [ -z $@ ]; then
-    echo "rm -rf /arnix/generations/${_next_generation}" > /arnix/etc/init.hooks/pre-undo_new_generation.hook
+    echo "rm -rf /arnix/generations/${_next_generation}" > /arnix/etc/init-hooks/pre-undo_new_generation.hook
+    echo "rm -f /arnix/etc/init-hooks/pre-undo_new_generation.hook" >> /arnix/etc/init-hooks/pre-undo_new_generation.hook
     
     log "Creating new generation ${_next_generation}"
     log "Cloning generation ${_current_generation}"
@@ -23,13 +24,13 @@ if [[ $@ != '*nocopy*' ]] 2>/dev/null || [ -z $@ ]; then
 fi
 
 if [[ $@ != '*nosymlink*' ]] 2>/dev/null || [ -z $@ ]; then
-    log "Activating generation ${_next_generation}"
     ln -srfT /arnix/generations/${_next_generation} /arnix/generations/current
     ln -srfT /arnix/generations/${_next_generation} /arnix/generations/latest
 
-    rm /arnix/etc/init.hooks/pre-undo_new_generation.hook
+    rm /arnix/etc/init-hooks/pre-undo_new_generation.hook
 fi
 if [[ $@ != '*nocopy*' ]] 2>/dev/null || [ -z $@ ]; then
+    log "Activating generation ${_next_generation}"
     for i in ${_dirs}; do
         /arnix/bin/busybox umount -l /$i
         /arnix/bin/busybox mount --bind /arnix/generations/${_next_generation}/$i /$i
