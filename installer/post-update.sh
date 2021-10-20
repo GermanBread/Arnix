437ba45a0173057549f9e58d03187acdc763d639
GermanBread <nd01@online.de>
2021-10-20 15:56:18 +0200

    toybox bug?

--- installer/post-update.sh
+++ installer/post-update.sh
@@ -1,5 +1,7 @@
 alias ln='/tmp/arnix-update/bin/toybox ln'
 
-command -v pacman &>/dev/null && \
-	ln -srfT /arnix/etc/100-arnix-change-symlink.hook /etc/pacman.d/hooks/100-arnix-change-symlink.hook
+if command -v pacman &>/dev/null; then
+	ln -srfT /arnix/etc/pacman-pre.hook /etc/pacman.d/hooks/0-arnix-create-generation.hook
+	ln -srfT /arnix/etc/pacman-post.hook /etc/pacman.d/hooks/100-arnix-change-symlink.hook
+fi
 ln -srfT /arnix/bin/arnixctl.sh /usr/bin/arnixctl
