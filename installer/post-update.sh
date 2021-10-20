be31651f185bf00bba74d6b63ebf389212468bd4
GermanBread <nd01@online.de>
2021-10-20 16:13:35 +0200

    distro checks

--- installer/post-update.sh
+++ installer/post-update.sh
@@ -3,8 +3,8 @@ alias ln='/tmp/arnix-update/bin/toybox ln'
 # toybox bug: if both files are the same amount of directories deep (or the destination is one higher), the resulting symlink is broken
 
 # Arch
-if command -v pacman &>/dev/null; then
+if [ -x /usr/bin/pacman ]; then
 	ln -sf ../../../arnix/etc/pacman-pre.hook /etc/pacman.d/hooks/0-arnix-create-generation.hook
 	ln -sf ../../../arnix/etc/pacman-post.hook /etc/pacman.d/hooks/100-arnix-change-symlink.hook
-	ln -sf ../../arnix/bin/arnixctl.sh /sbin/arnixctl
+	ln -sf ../../arnix/bin/arnixctl.sh /usr/bin/arnixctl
 fi
