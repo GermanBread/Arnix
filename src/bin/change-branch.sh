0c4be972d69c85647d4c0e0f9440cb77a00466aa
GermanBread <nd01@online.de>
2022-04-08 14:36:34 +0200

    add new branch + release prep

--- src/bin/change-branch.sh
+++ src/bin/change-branch.sh
@@ -14,7 +14,7 @@ fi
 
 echo 'Select branch:'
 echo '1 - stable'
-echo '2 - unstable'
+echo '2 - dev'
 echo '3 - custom'
 while ! [[ $_mode = [123] ]] 2>/dev/null; do
     printf '\0337'
@@ -25,16 +25,16 @@ echo
 
 case ${_mode} in 
     1)
-        sed -Ei "s,_update_source_tarball=.*,_update_source_tarball=https://github.com/germanbread/arnix/releases/latest/download/arnix-bootstrap.tar.gz," /arnix/arnix.conf
-        sed -Ei "s,_update_source_checksum=.*,_update_source_checksum=https://github.com/germanbread/arnix/releases/latest/download/arnix-bootstrap.sha1sum," /arnix/arnix.conf
-        sed -Ei "s,_branch_preset=.*,_branch_preset=stable," /arnix/arnix.conf
+        sed -Ei "s,_update_source_tarball=.*,_update_source_tarball=https://raw.githubusercontent.com/GermanBread/Arnix/stable/installer/arnix-bootstrap.tar.gz," /arnix/arnix.conf
+        sed -Ei "s,_update_source_checksum=.*,_update_source_checksum=https://raw.githubusercontent.com/GermanBread/Arnix/stable/installer/arnix-bootstrap.sha1sum," /arnix/arnix.conf
+        sed -Ei "s,_branch_preset=.*,_branch_preset=unstable," /arnix/arnix.conf
         log "Branch changed to 'stable'"
     ;;
     2)
-        sed -Ei "s,_update_source_tarball=.*,_update_source_tarball=https://raw.githubusercontent.com/GermanBread/Arnix/master/installer/arnix-bootstrap.tar.gz," /arnix/arnix.conf
-        sed -Ei "s,_update_source_checksum=.*,_update_source_checksum=https://raw.githubusercontent.com/GermanBread/Arnix/master/installer/arnix-bootstrap.sha1sum," /arnix/arnix.conf
+        sed -Ei "s,_update_source_tarball=.*,_update_source_tarball=https://raw.githubusercontent.com/GermanBread/Arnix/dev/installer/arnix-bootstrap.tar.gz," /arnix/arnix.conf
+        sed -Ei "s,_update_source_checksum=.*,_update_source_checksum=https://raw.githubusercontent.com/GermanBread/Arnix/dev/installer/arnix-bootstrap.sha1sum," /arnix/arnix.conf
         sed -Ei "s,_branch_preset=.*,_branch_preset=unstable," /arnix/arnix.conf
-        log "Branch changed to 'unstable'"
+        log "Branch changed to 'dev'"
     ;;
     3)
         question 'Please paste the link to the arnix-bootstrap.tar.gz file here:'
