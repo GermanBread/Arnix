1d442b27330d6d389f7fc529e4536c11ec3a7247
GermanBread <nd01@online.de>
2022-06-29 14:55:25 +0200

    update website

--- website/status.ts
+++ website/status.ts
@@ -1,18 +1,11 @@
-function replaceTemporaryFields() {
-    var ticker : HTMLElement = document.getElementById("ticker") ?? document.body // see script.ts
+async function replaceTemporaryFields() {
+    var ticker : HTMLElement = document.getElementById("ticker")!;
 
-    fetch('https://api.github.com/repos/GermanBread/Arnix/releases/latest')
-        .then((res) => {
-            res.json().then((json) => {
-                ticker.innerHTML = ticker.innerHTML.replace("%s", json.tag_name.link(json.html_url))
-            })
-        })
-        .catch((err) => console.error(err))
-    fetch('https://api.github.com/repos/GermanBread/Arnix/commits/master')
-        .then((res) => {
-            res.json().then((json) => {
-                ticker.innerHTML = ticker.innerHTML.replace("%g", json.sha.link(json.html_url))
-            })
-        })
-        .catch((err) => console.error(err))
+    const stabledata = await fetch('https://api.github.com/repos/GermanBread/Arnix/releases/latest')
+    const stablejson = await stabledata.json();
+    ticker.innerHTML = ticker.innerHTML.replace("%s", stablejson.tag_name.link(stablejson.html_url))
+
+    const devdata = await fetch('https://api.github.com/repos/GermanBread/Arnix/commits/dev')
+    const devjson = await devdata.json();
+    ticker.innerHTML = ticker.innerHTML.replace("%g", devjson.sha.link(devjson.html_url))
 }
