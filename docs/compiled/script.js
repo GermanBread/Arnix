13f8c55b251ea30c1830b8f9964683d41eb956fd
GermanBread <nd01@online.de>
2023-05-05 15:30:18 +0200

    do not forger to deploy

--- docs/compiled/script.js
+++ docs/compiled/script.js
@@ -1,9 +1,10 @@
 var logoScroll = 60;
+var logoElement;
 function init() {
+    var _a;
+    logoElement = (_a = document.getElementById("logo")) !== null && _a !== void 0 ? _a : document.body; // Stop annoying me VSCode. This is to shut you up
     window.addEventListener("scroll", handlescroll);
 }
 function handlescroll() {
-    var _a;
-    var element = (_a = document.getElementById("logo")) !== null && _a !== void 0 ? _a : document.body; // Stop annoying me VSCode. This is to shut you up
-    element.style.opacity = ((logoScroll - window.scrollY) / logoScroll).toString();
+    logoElement.style.opacity = ((logoScroll - window.scrollY) / logoScroll).toString();
 }
